80b0a9f346df2f99cc7291a4ad7706d50e25360d
jthhn <jithinam530@gmail.com>
2025-05-24 17:49:15 +0530

    Configured docker compose and checks to wait for db

--- app/app/settings.py
+++ app/app/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
-        'PASSWORD': os.environ.get('DB_PASS'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
     }
 }
 
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,26 +1,29 @@
 """
-Django command to wait for the database to be available.
+Django command to wait for db to be available
 """
+
 import time
-from psycopg2 import OperationalError as Psycopg2Error
+
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    """Django command to wait for database."""
+    # Django command to wait for db
 
     def handle(self, *args, **options):
-        """Entrypoint for command."""
+        """Entrypoint for command"""
         self.stdout.write('Waiting for database...')
         db_up = False
         while db_up is False:
             try:
                 self.check(databases=['default'])
                 db_up = True
-            except (Psycopg2Error, OperationalError):
-                self.stdout.write('Databases unavailable, waiting 1 second...')
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Databases unavailable, \
+                    waiting for 1 second...')
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
